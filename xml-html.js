98dabe305a6c4e4fadb137ac38e62bcb76939e5e
adi555j <adi555j@gmail.com>
2017-11-14 16:21:49 +0530

    Update xml-html.js

--- xml-html.js
+++ xml-html.js
@@ -12,8 +12,13 @@ window.onload = function(){
 
 //get xml dat from xml file
 var catchXml = function(){
-			var xhttp = new XMLHttpRequest();
-				xhttp.onreadystatechange = function(){
+		    	var xhttp = "";
+			if (window.XMLHttpRequest) {
+				xhttp = new XMLHttpRequest();
+			} else {
+				xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xhttp.onreadystatechange = function(){
 				if(this.readyState == 4 && this.status == 200){
 				listOfAttributes = [
 					'server-name',
